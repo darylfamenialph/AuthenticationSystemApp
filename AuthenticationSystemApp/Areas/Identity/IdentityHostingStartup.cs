89af87b521eb9031c223528f2d07f47ab68e18c6
Kim Daryl Famenial <darylfamenialph@gmail.com>
2020-09-08 16:16:47 +0800

    Add project files.

---
+++ AuthenticationSystemApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using AuthenticationSystemApp.Areas.Identity.Data;
+using AuthenticationSystemApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AuthenticationSystemApp.Areas.Identity.IdentityHostingStartup))]
+namespace AuthenticationSystemApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequiredLength = 4;
+                    })
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
